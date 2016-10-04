ee00f72972867cafde096dd09ea08b36c0d8feb7
Brendon <brennon.mckeever@gmail.com>
2016-10-04 16:18:44 -0500

    Initial commit

---
+++ app/src/main/java/com/clara/movielistviewwithcursoradapter/DatabaseManager.java
@@ -0,0 +1,114 @@
+package com.clara.movielistviewwithcursoradapter;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseManager {
+
+	private Context context;
+	private SQLHelper helper;
+	private SQLiteDatabase db;
+
+	protected static final String DB_NAME = "movies";
+	protected static final int DB_VERSION = 1;
+	protected static final String DB_TABLE = "ratings";
+
+	protected static final String ID_COL = "_id";
+	protected static final String MOVIE_NAME_COL = "name";
+	protected static final String MOVIE_RATING_COL = "rating";
+
+	private static final String DB_TAG = "DatabaseManager" ;
+	private static final String SQLTAG = "SQLHelper" ;
+
+	public DatabaseManager(Context c) {
+		this.context = c;
+		helper = new SQLHelper(c);
+		this.db = helper.getWritableDatabase();
+	}
+
+	public void close() {
+		helper.close(); //Closes the database - very important!
+	}
+
+
+	public Cursor getAllMovies() {
+
+		Cursor cursor = db.query(DB_TABLE, null, null, null, null, null, MOVIE_NAME_COL);
+		return cursor;
+	}
+
+
+
+	public boolean updateRating(int movieID, float rating) {
+
+		Log.d(DB_TAG, "About to update rating for: " + movieID + " to " + rating);
+
+		ContentValues updateValues = new ContentValues();
+		updateValues.put(MOVIE_RATING_COL, rating);
+
+		String where = ID_COL + " = ? ";
+		String[] whereArgs = { Integer.toString(movieID) };
+
+		int rowsMod = db.update(DB_TABLE, updateValues, where, whereArgs);
+
+		if (rowsMod == 1 ) {
+
+			return true;
+
+		} else {
+			return false;
+		}
+
+	}
+
+	//Add a product and quantity to the database.
+	// Returns true if movie added, false if movie is already in the database
+	public boolean addMovie(String name, float rating) {
+
+		ContentValues newMovie = new ContentValues();
+		newMovie.put(MOVIE_NAME_COL, name);
+		newMovie.put(MOVIE_RATING_COL, rating);
+
+		try {
+			db.insertOrThrow(DB_TABLE, null, newMovie);
+			Log.d(DB_TAG, "Added new movie " + name + " with rating " + rating);
+
+			return true;
+
+		} catch (SQLiteConstraintException sqlce) {
+
+			Log.e(DB_TAG, "Error inserting into table " + " Name: " + name +
+			" Rating: " + rating, sqlce);
+
+			return false;
+		}
+
+
+	}
+
+
+	public class SQLHelper extends SQLiteOpenHelper {
+		public SQLHelper(Context c){
+			super(c, DB_NAME, null, DB_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			String createSQLbase = "CREATE TABLE %s ( %s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT UNIQUE, %s FLOAT )";
+			String createSQL = String.format(createSQLbase, DB_TABLE, ID_COL, MOVIE_NAME_COL, MOVIE_RATING_COL);
+			db.execSQL(createSQL);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
+			onCreate(db);
+			Log.w(SQLTAG, "Upgrade table - drop and recreate it");
+		}
+	}
+}
---
+++ app/src/main/java/com/clara/movielistviewwithcursoradapter/MovieCursorAdapter.java
@@ -0,0 +1,72 @@
+package com.clara.movielistviewwithcursoradapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+
+public class MovieCursorAdapter extends CursorAdapter {
+
+	private static final String TAG = "MOVIE CURSOR ADAPTER";
+	RatingChangedListener ratingChangedListener;
+
+	//Correpond to column numbers in database
+	private static final int ID_COL = 0;
+	private static final int MOVIE_COL = 1;
+	private static final int RATING_COL = 2;
+
+	public MovieCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+		super(context, c, autoRequery);
+
+		//Convert context to RatingChangedListener, just like adding a listener to a Fragment
+		if (context instanceof RatingChangedListener) {
+			this.ratingChangedListener = (RatingChangedListener) context;
+		} else {
+			throw new RuntimeException(context.toString() + " must implement RatingChangedListener");
+		}
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View v = inflater.inflate(R.layout.movie_list_item, parent, false);
+		return v;
+	}
+
+	@Override
+	public void bindView(View view, Context context, final Cursor cursor) {
+
+		TextView nameTV = (TextView) view.findViewById(R.id.movie_title_list_text_view);
+		RatingBar ratingbar = (RatingBar) view.findViewById(R.id.movie_rating_list_rating_bar);
+
+		nameTV.setText(cursor.getString(MOVIE_COL));
+		ratingbar.setRating(cursor.getFloat(RATING_COL));
+
+		final int movie_id = cursor.getInt(ID_COL); // Need this to update data.
+
+		ratingbar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+			@Override
+			public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+				if (fromUser) {
+
+					ratingChangedListener.notifyRatingChanged(movie_id, rating);
+
+				}
+
+			}
+		});
+
+	}
+
+
+	interface RatingChangedListener {
+		void notifyRatingChanged(int movieID, float newRating);
+	}
+}
